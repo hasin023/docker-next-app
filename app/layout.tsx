2231d19d57ad9eee6ec9a1953234889dee4e19df
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2024-11-30 12:09:46 +0600

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 })
 export const metadata: Metadata = {
   title: "Docker NextJS",
-  description: "Dockerize NextJS App",
+  description: "Dockerize NextJS App trying to host on coolify",
 }
 
 export default function RootLayout({
